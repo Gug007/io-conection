c6cdb9459393b9d13ca0c3cb2468e81c9d206568
gug007 <gug007@i.ua>
2016-08-08 17:37:04 +0300

    service frontend - add counter

--- src/service/server.js
+++ src/service/server.js
@@ -11,15 +11,17 @@ const ioServer = io(server);
 app.use('/static', express.static(path.join(__dirname, '../../public/scripts')));
 
 app.get('*', (req, res) => {
-  res.send('Hello World')
+  res.sendFile(path.join(__dirname, '../../public/service', 'index.html'))
 })
 
 server.listen(SERVICE_PORT);
 
 ioServer.on('connection', (socket) => {
   socket.on('message', (data) => {
+    socket.broadcast.emit('message for service', { data: data.data });
     console.log("<<", data.data);
   });
+
   // Disconnect listener
   socket.on('disconnect', () => console.log('service: client disconnected'))
 }); 
